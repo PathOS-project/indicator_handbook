fa40bf23976ea5302c1331ee42cf990918f097d1
Vincent Traag <vincent@traag.net>
2023-06-15 22:27:47 +0200

    Changed script to be able to process multiple files.

--- indicator_templates/src/convert_word_to_quarto.sh
+++ indicator_templates/src/convert_word_to_quarto.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-DOCX_FILE=$1
-
 # Get script dir
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters" >&2
-    echo "Usage: convert_word_to_quarto.sh <docx file>" >&2
-    exit 2
-fi
+for DOCX_FILE in "$@"
+do
 
-if [ ! -f "${DOCX_FILE}" ]; then
-    echo "${DOCX_FILE} does not exist." >&2
-    exit 3
-fi
+    if [ ! -f "${DOCX_FILE}" ]; then
+        echo "${DOCX_FILE} does not exist." >&2
+        exit 3
+    fi
 
-echo "Converting ${DOCX_FILE} to ${DOCX_FILE%.*}.qmd"
+    echo "Converting ${DOCX_FILE} to ${DOCX_FILE%.*}.qmd"
 
-mammoth \
-    "${DOCX_FILE}" \
-    --style-map=${SCRIPT_DIR}/indicators-mammoth-style-map.txt \
-| \
-pandoc \
-    -f html \
-    -t gfm+backtick_code_blocks \
-    -o "${DOCX_FILE%.*}.qmd"
+    mammoth \
+        "${DOCX_FILE}" \
+        --style-map=${SCRIPT_DIR}/indicators-mammoth-style-map.txt \
+    | \
+    pandoc \
+        -f html \
+        -t gfm+backtick_code_blocks \
+        -o "${DOCX_FILE%.*}.qmd"
+
+done
