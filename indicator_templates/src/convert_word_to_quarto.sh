6d2aeeb3c4d1f9e6396a05f7eb440d05458abd33
Vincent Traag <vincent@traag.net>
2023-06-15 22:23:22 +0200

    Created proper conversion script.

--- indicator_templates/src/convert_word_to_quarto.sh
+++ indicator_templates/src/convert_word_to_quarto.sh
@@ -1,10 +1,28 @@
-mammoth \
-    --style-map=indicators-mammoth-style-map.txt \
-    --output-dir "html" \
-    "Open Access uptake.docx"
+#!/bin/bash
 
+DOCX_FILE=$1
+
+# Get script dir
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters" >&2
+    echo "Usage: convert_word_to_quarto.sh <docx file>" >&2
+    exit 2
+fi
+
+if [ ! -f "${DOCX_FILE}" ]; then
+    echo "${DOCX_FILE} does not exist." >&2
+    exit 3
+fi
+
+echo "Converting ${DOCX_FILE} to ${DOCX_FILE%.*}.qmd"
+
+mammoth \
+    "${DOCX_FILE}" \
+    --style-map=${SCRIPT_DIR}/indicators-mammoth-style-map.txt \
+| \
 pandoc \
     -f html \
     -t gfm+backtick_code_blocks \
-    -o "md/Open Access uptake.md" \
-    "html/Open Access uptake.html"
+    -o "${DOCX_FILE%.*}.qmd"
